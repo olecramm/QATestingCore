22d1a551d434a3da7e4deee011b38027cf4a091a
Marcelo Moreira <Marcelo.Moreira@capco.com>
2019-06-01 14:09:51 -0300

    alteracao do metodo de validacao json schema

--- QATestingCore.IntegratedTests/Assertions/ResponseContentAssertions.cs
+++ QATestingCore.IntegratedTests/Assertions/ResponseContentAssertions.cs
@@ -7,15 +7,6 @@ namespace QATestingCore.IntegratedTests.Assertions
 {
     public static class ResponseContentAssertions
     {
-        /// <summary>
-        /// Compare the statuscode received to the statuscode expected
-        /// </summary>
-        /// <param name="statusCode">Represents the status code got from response being validated</param>
-        /// <param name="expectedStatusCode">Represents the expected status code</param>
-        /// <returns>Returns a boolean data to be asserted</returns>
-        public static bool AssertStatusCode(HttpStatusCode statusCode, HttpStatusCode expectedStatusCode)
-        {
-            return statusCode == expectedStatusCode;
-        }
+
     }
 }
