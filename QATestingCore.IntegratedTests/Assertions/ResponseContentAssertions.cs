8a964aad9a97dd7855ba12f992d160b2bb17fcf9
Marcelo Moreira <Marcelo.Moreira@capco.com>
2019-06-12 08:23:29 -0300

    desenvolvimento do metodo valida dados de resposta

--- QATestingCore.IntegratedTests/Assertions/ResponseContentAssertions.cs
+++ QATestingCore.IntegratedTests/Assertions/ResponseContentAssertions.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
 using System.Collections.Generic;
-using System.Net;
-using System.Text;
 
 namespace QATestingCore.IntegratedTests.Assertions
 {
     public static class ResponseContentAssertions
     {
+        public static bool AssertResponseData(IRestResponse response, IDictionary<string, string> expectedResult, out string resultTestMessage)
+        {
+            resultTestMessage = string.Empty;
 
+            var responseContent = JObject.Parse(response.Content);            
+
+            foreach (KeyValuePair<string,string> item in expectedResult)
+            {
+                var Key = responseContent.GetValue(item.Key);
+
+                if (responseContent.GetValue(item.Key) == null)
+                {
+                    resultTestMessage = $"Parameter {item.Key} is not present";
+
+                    return false;
+                }
+                else if (responseContent.GetValue(item.Key).Equals(item.Value))
+                {
+                    resultTestMessage = $"Parameter {item.Key} value is not equal as expected";
+
+                    return false;
+                }                
+            };
+            return true;
+        }
     }
 }
